04850eef5bbfbbb3b4f813a592a9ad775ad01e1a
shengaoyoushii <ucsantacruzxiaochenzhuo@gmail.com>
2016-10-12 00:15:37 -0400

    form validation

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -7,6 +7,18 @@ class SignUpForm(forms.ModelForm):
 		model = SignUp
 		fields = ['full_name','email']
 
-	def clean_email(self):
-		print self.cleaned_data.get('email')
-		return 'abc@gmail.com'
+
+	# def clean_email(self):
+	# 	email = self.cleaned_data.get('email')
+	# 	email_base, provider = email.split("@")
+	# 	domain, extension = provider.split('.')
+	# 	if not domain == 'USC':
+	# 		raise forms.ValidationError("Please make sure you use your USC email.")
+	# 	if not extension == "edu":
+	# 		raise forms.ValidationError("Please use a valid .EDU email address")
+	# 	return email
+
+	# def clean_full_name(self):
+	# 	full_name = self.cleaned_data.get('full_name')
+	# 	#write validation code.
+	# 	return full_name
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import SignUpForm
 
 def home(request):
-	return render(request,"home.html",{})
+	if request.method == "POST":
+		# print request.POST
+	title = "Jiahui Wu is a Pig"
+	form = SignUpForm(request.POST or None)
+	context = {
+		"title":title,
+		"form":form,
+
+	}
+	return render(request,"home.html",context)
