0a4262014bb6a47288f0dae9c907df15d36ca590
shengaoyoushii <ucsantacruzxiaochenzhuo@gmail.com>
2016-10-19 15:18:04 -0400

    home page template

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+import random
+from videos.models import Video
+
+
 
 # Create your views here.
-from .forms import SignUpForm
-from videos.models import Video
 
 def home(request):
 	# if request.method == "POST":
 	# 	# print request.POST
 	title = "Welcome to my site"
 	form = SignUpForm(request.POST or None)
-	videos = Video.objects.all()
+
+	random_idx = random.randint(0, Video.objects.count() - 1)
+	random_video = Video.objects.all()[random_idx]
+	random_videos = Video.objects.all().order_by('?')[:4]
 	context = {
 		"title":title,
 		"form":form,
-		"videos":videos
-
+		"random_video":random_video,
+		"random_videos":random_videos,
 	}
 	return render(request,"home.html",context)
